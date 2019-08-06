0897038409fdaeb3d844c66f5c9733a65372cafa
Lucia Quirke <lqui376@aucklanduni.ac.nz>
2019-08-06 15:59:23 +1200

    clean up of sleep apnea binary classifier branch

--- openData.py
+++ openData.py
@@ -1,18 +1,17 @@
-#NOTES
-#print(annsamp.__dict__)        #can be used to list attributes and values of object
-#fs = sig.fs                    #can be used to find sampling rate of 250 Hz
+# NOTES
+# print(annsamp.__dict__)        #can be used to list attributes and values of object
+# fs = sig.fs                    #can be used to find sampling rate of 250 Hz
 
-from IPython.display import display
 import numpy as np
 import os
 import wfdb
 import csv
 from sklearn import preprocessing
 
-#change to current OS
-operatingSystem = 'windows'
+# change to current OS
+operatingSystem = 'macOS'
 
-if operatingSystem == 'linux':
+if operatingSystem is 'linux' or 'macOS':
     dataPath = '/data/mit-bih-polysomnographic-database-1.0.0/'
     inputsPath = '/data/inputs/'
     targetsPath = '/data/targets/'
@@ -24,21 +23,17 @@ else:
 recordList = wfdb.get_record_list('slpdb')
 apneaLabels = np.array(['H', 'HA', 'OA', 'CA', 'CAA', 'X'])
 
-inputArray = np.empty((0,1))
-outputArray = np.empty((0,1))
-recordNumber = 1
+for recordNumber, record in enumerate(recordList):
 
-for record in recordList:
-
-    #Read the annotations 
+    # Read the annotations
     annsamp = wfdb.rdann(os.getcwd() + dataPath + record, extension='st', summarize_labels=True)
     
-    #Read the data, create a Record
+    # Read the data, create a Record
     sig = wfdb.rdrecord(os.getcwd() + dataPath + record, channels=[2]) 
     
     actualPSignal = sig.p_signal
     
-    #binarize data: 1 for a sleep apnea event in the epoch
+    # binarize data: 1 for a sleep apnea event in the epoch
     for idx, event in enumerate(annsamp.aux_note): 
         for x in apneaLabels:
             if x in event:
@@ -47,32 +42,27 @@ for record in recordList:
             else:
                 annsamp.aux_note[idx] = 0
 
-    #remove unannotated epochs from start of record
+    # remove unannotated epochs from start of record
     numberEpochs = len(actualPSignal)/7500
     if numberEpochs != len(annsamp.aux_note):
         startingIndex = int(numberEpochs - len(annsamp.aux_note))
         actualPSignal = actualPSignal[startingIndex*7500:]
 
-    #standardise signal
+    # standardise signal
     actualPSignal = preprocessing.scale(actualPSignal)
 
-    #write each epoch to a csv file, named by record number and epoch number
+    # write each epoch to a csv file, named by record number and epoch number
     trimmedNumberEpochs = len(actualPSignal)/7500
     print(trimmedNumberEpochs)
     epochs = np.split(actualPSignal, trimmedNumberEpochs)
-    index = 1
 
-    for epoch in epochs:
-        #save input and output arrays as csv files
-        with open(os.getcwd() + inputsPath + str(recordNumber) + '_' + str(index) + '.csv','w') as filehandler:
-            csvWriter = csv.writer(filehandler,delimiter=' ')
+    for epochNumber, epoch in enumerate(epochs):
+        # save input and output arrays as csv files
+        with open(os.getcwd() + inputsPath + str(recordNumber) + '_' + str(epochNumber) + '.csv', 'w') as filehandler:
+            csvWriter = csv.writer(filehandler, delimiter=' ')
             csvWriter.writerows(epoch) 
 
-            #write target values to csv files, named by record number and epoch number
-        with open(os.getcwd() + targetsPath + str(recordNumber) + '_' + str(index) + ".csv","w") as filehandler:
-            csvWriter = csv.writer(filehandler,delimiter=' ')
-            csvWriter.writerow(str(annsamp.aux_note[index-1]))
-        
-        index = index + 1
-
-    recordNumber = recordNumber + 1
+            # write target values to csv files, named by record number and epoch number
+        with open(os.getcwd() + targetsPath + str(recordNumber) + '_' + str(epochNumber) + ".csv", "w") as filehandler:
+            csvWriter = csv.writer(filehandler, delimiter=' ')
+            csvWriter.writerow(str(annsamp.aux_note[epochNumber-1]))
